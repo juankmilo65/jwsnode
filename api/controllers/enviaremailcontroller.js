a83803d594352effd1970f107a829614d529df8e
Juan Camilo <juankmilo65@hotmail.com>
2019-09-27 15:14:58 +0300

    Update enviaremailcontroller.js

--- api/controllers/enviaremailcontroller.js
+++ api/controllers/enviaremailcontroller.js
@@ -7,11 +7,6 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-Tarjeta Santander Mexico, 5579070089317740, por favor 🙏🏼 no me comentes más de pagos externos por la aplicación yo debo decirte no por allá, por que la ultima vez me bloquearon, es la única forma donde consigo clientes, yo soy freelance, ademas me sirve la calificación por estatus, entonces si pueedes cambiar las condiciones del proyecto y cerrarlo cosa que no me afecte a mi, así sea pago de un dólar porfa 🙏🏼 y ponerme calificación, sería súper 
-
-Para cerrar elproyecto de manera saticfactoria y que nos sirva a los 2 entonces, ponle no se un dolar a workana, cierra el proyecto or ahi 
-para que me puedas calificar, eso me sirve y el exedente me lo pasas a la tarjeta, pero no escribas nada por el char, abajo etában mis correos.
-
 var mailOptions = {
   from: "juankmilo65@hotmail.com",
   to: "darkfire65@gmail.com",
